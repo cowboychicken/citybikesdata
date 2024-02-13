ead17a42df91f20c0b40e9dff280f53f9cee9f01
cowboychicken <aaroncim@gmail.com>
2024-02-13 15:25:26 -0600

    change start date

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -5,24 +5,25 @@ from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 
 
-@dag(dag_id = "citybikes_etl", schedule_interval="*/5 * * * *")
+@dag(dag_id = "citybikes_etl", schedule_interval="*/5 * * * *", start_date=days_ago(0))
 def citybikes_etl_dag():
     
-    @task(task_id="load_json_to_edl", start_date=days_ago(1))
+    @task(task_id="load_json_to_edl")
     def extract_from_api(ds=None, **kwargs):
         return extract.main()
     
-    @task(task_id="process_networks", start_date=days_ago(1))
+    @task(task_id="process_networks")
     def process_networks(ds=None, **kwargs):
         return transform.load_network_data_to_edw()
     
-    @task(task_id="process_stations", start_date=days_ago(1))
+    @task(task_id="process_stations")
     def process_stations(ds=None, **kwargs):
         return transform.load_station_data_to_edw()
 
-    dummytask = extract_from_api()
-    dummytask = process_networks()
-    dummytask = process_stations()
+    #dummytask = extract_from_api()
+    #dummytask = process_networks()
+    #dummytask = process_stations()
+    extract_from_api() >> process_networks() >> process_stations()
 
 citybikes_dag = citybikes_etl_dag()
 
