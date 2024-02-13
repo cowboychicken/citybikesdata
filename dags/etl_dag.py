c7a9492e040ebe7dd8e0d63e0cfcb3bcb2f17fdb
cowboychicken <aaroncim@gmail.com>
2024-02-13 14:36:46 -0600

    change airflow interval

--- dags/etl_dag.py
+++ dags/etl_dag.py
@@ -8,15 +8,15 @@ from airflow.utils.dates import days_ago
 @dag(dag_id = "citybikes_etl", schedule_interval="*/5 * * * *")
 def citybikes_etl_dag():
     
-    @task(task_id="load_json_to_edl", start_date=days_ago(1))
+    @task(task_id="load_json_to_edl")
     def extract_from_api(ds=None, **kwargs):
         return extract.main()
     
-    @task(task_id="process_networks", start_date=days_ago(1))
+    @task(task_id="process_networks")
     def process_networks(ds=None, **kwargs):
         return transform.load_network_data_to_edw()
     
-    @task(task_id="process_stations", start_date=days_ago(1))
+    @task(task_id="process_stations")
     def process_stations(ds=None, **kwargs):
         return transform.load_station_data_to_edw()
 
