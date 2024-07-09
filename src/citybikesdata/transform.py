f796ffe0c87d86bd9504e813f7c8cd159d70c52c
cowboychicken <aaroncim@gmail.com>
2024-07-09 14:08:04 -0500

    major change for T&L

--- src/citybikesdata/transform.py
+++ src/citybikesdata/transform.py
@@ -12,18 +12,71 @@ from citybikesdata.utils.db_config import get_db_creds as db_creds
 #       - remove brackets and quotes from 'company'. EX) ['MyCompanyName'] -- > MyCompanyName
 #       - split up 'location' EX) {'city': 'Moscow', 'country': 'RU', 'latitude': 55.75, 'longitude': 37.616667}
 #           into 'city' ,'country', 'latitude', 'longitude'
+
+
+def fetch_db_data(conn, table, columns, filter):
+    if filter == None:
+        filter = True
+    query = f"SELECT {','.join(columns)} FROM {table} WHERE {filter};"
+    with conn.cursor() as curs:
+        curs.execute(query)
+        return pd.DataFrame(curs.fetchall(), columns=columns)
+
+def stations_in_edw():
+
+    columns = [
+        'id',
+        'name',
+        'extra',
+        'latitude',
+        'longitude',
+        'timestamp',
+        'free_bikes',
+        'empty_slots',
+        'network',
+        'dateApiCalled',
+        'dateAdded',
+    ]
+
+    try:
+        with DBConnection(db_creds()).conn as conn:
+            df_stations = fetch_db_data(conn, 'citybikes.stations', columns, filter="timestamp_custom_bin_hour is null")
+    except Exception as e:
+        logging.error(traceback.format_exc())
+    print("YO")
+    print(f"[transform.stations_in_edw()] df_stations.count = {df_stations.count}")
+
+
+    # Add bin for timestamp 
+    tz_offset = 5
+    df_stations['timestamp_custom_bin_hour'] = (df_stations['timestamp'].dt.hour - tz_offset) % 24
+
+    print("[transform.stations_in_edw()] df_stations.'timestamp_custom_bin_hour' = \n", df_stations[['timestamp','timestamp_custom_bin_hour']])
+
+     # upsert records
+    tuples = [tuple(x) for x in df_stations.to_numpy()]
+    df_stations_columns = ','.join(list(df_stations.columns))
+    for row in tuples:
+        try:
+            with DBConnection(db_creds()).conn as conn:
+                with conn.cursor() as curs:
+                    #query_string = f"INSERT INTO citybikes.networks ({df_networks_columns}) VALUES %s ON CONFLICT(id) DO UPDATE SET company_formatted=EXCLUDED.company_formatted, location_city=EXCLUDED.location_city, location_country=EXCLUDED.location_country, location_latitude=EXCLUDED.location_latitude, location_longitude=EXCLUDED.location_longitude;"
+                    query_string = f"INSERT INTO citybikes.stations ({df_stations_columns}) VALUES %s ON CONFLICT(id,timestamp) DO UPDATE SET timestamp_custom_bin_hour=EXCLUDED.timestamp_custom_bin_hour;"
+                    curs.execute(query_string, (row,))
+        except Exception as e:
+            logging.error(traceback.format_exc())
+            return
+
+
+
 def networks_in_edw():
+    
     columns = [
         'id',
         'href',
         'name',
         'company',
-        'company_formatted',
         'location',
-        'location_city',
-        'location_country',
-        'location_latitude',
-        'location_longitude',
         'source',
         'gbfs_href',
         'license',
@@ -31,33 +84,16 @@ def networks_in_edw():
         'dateApiCalled',
         'dateAdded',
     ]
-    query_results = None
-    with DBConnection(db_creds()).conn as conn:
-        try:
-            with conn.cursor() as curs:
-                curs.execute(
-                    "SELECT " + ','.join(columns) + " FROM citybikes.networks;"
-                )
-                query_results = curs.fetchall()
-        except Exception as e:
-            print(logging.error(traceback.format_exc()))
 
-    df_networks = pd.DataFrame(query_results, columns=columns)
-    df_networks = df_networks[
-        [
-            'id',
-            'href',
-            'name',
-            'company',
-            'location',
-            'source',
-            'gbfs_href',
-            'license',
-            'ebikes',
-            'dateApiCalled',
-            'dateAdded',
-        ]
-    ]
+    try:
+        with DBConnection(db_creds()).conn as conn:
+            df_networks = fetch_db_data(
+                conn, 'citybikes.networks', columns, filter=None
+            )
+    except Exception as e:
+        logging.error(traceback.format_exc())
+
+    print(f"[transform.networks_in_edw()] df_networks.count = {df_networks.count}")
 
     # clean company column
     df_networks['company_formatted'] = df_networks['company'].map(
@@ -74,17 +110,17 @@ def networks_in_edw():
     tuples = [tuple(x) for x in df_networks.to_numpy()]
     df_networks_columns = ','.join(list(df_networks.columns))
     for row in tuples:
-        with DBConnection(db_creds()).conn as conn:
-            try:
+        try:
+            with DBConnection(db_creds()).conn as conn:
                 with conn.cursor() as curs:
-                    query_string = (
-                        "INSERT INTO citybikes.networks ("
-                        + df_networks_columns
-                        + ") VALUES %s ON CONFLICT(id) DO UPDATE SET company_formatted=EXCLUDED.company_formatted, location_city=EXCLUDED.location_city, location_country=EXCLUDED.location_country, location_latitude=EXCLUDED.location_latitude, location_longitude=EXCLUDED.location_longitude;"
-                    )
+                    #query_string = f"INSERT INTO citybikes.networks ({df_networks_columns}) VALUES %s ON CONFLICT(id) DO UPDATE SET company_formatted=EXCLUDED.company_formatted, location_city=EXCLUDED.location_city, location_country=EXCLUDED.location_country, location_latitude=EXCLUDED.location_latitude, location_longitude=EXCLUDED.location_longitude;"
+                    query_string = f"INSERT INTO citybikes.networks ({df_networks_columns}) VALUES %s ON CONFLICT(id,dateAdded) DO UPDATE SET company_formatted=EXCLUDED.company_formatted;"
                     curs.execute(query_string, (row,))
-            except Exception as e:
-                print(logging.error(traceback.format_exc()))
+        except Exception as e:
+            logging.error(traceback.format_exc())
+            return
+
+
     print(
         "[transform.networks_in_edw()] 'company' and 'location' formatting complete."
     )
@@ -92,7 +128,7 @@ def networks_in_edw():
 
 def run():
     networks_in_edw()
-
+    stations_in_edw()
 
 if __name__ == "__main__":
     run()
