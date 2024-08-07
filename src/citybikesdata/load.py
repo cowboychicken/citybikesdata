b0b55f9fd84655a7d0383c5b0736539f871cbe36
cowboychicken <aaroncim@gmail.com>
2024-08-07 14:10:18 -0500

    fix load issue

--- src/citybikesdata/load.py
+++ src/citybikesdata/load.py
@@ -22,7 +22,7 @@ def fetch_updates(conn, message):
 
 
 def fetch_db_data(conn, table, columns, filter):
-    if filter == None:
+    if filter is None:
         filter = True
     query = f"SELECT {','.join(columns)} FROM {table} WHERE {filter};"
     with conn.cursor() as curs:
@@ -31,16 +31,15 @@ def fetch_db_data(conn, table, columns, filter):
 
 
 def fetch_station_db_data(conn, table, columns, filter):
-    if filter == None:
+    if filter is None:
         filter = True
-    query = f"SELECT s1.id, s2.free_bikes, s2.empty_slots FROM (SELECT id,max(dateApiCalled) as maxdate FROM citybikes.stations WHERE {filter} GROUP BY id) as s1 LEFT JOIN (SELECT id,dateApiCalled,free_bikes, empty_slots FROM citybikes.stations) as s2 ON s1.id=s2.id AND s1.maxdate=s2.dateApiCalled ;"
+    query = f"SELECT s1.id, s2.free_bikes, s2.empty_slots FROM (SELECT id,max(dateApiCalled) as maxdate FROM citybikes.stations WHERE {filter} GROUP BY id) as s1 LEFT JOIN citybikes.stations as s2 ON s1.id=s2.id AND s1.maxdate=s2.dateApiCalled;"
     with conn.cursor() as curs:
         curs.execute(query)
         return pd.DataFrame(curs.fetchall(), columns=columns)
 
 
 def insert_updates(df_differences, update, table):
-
     # if df_differences.empty: return
     tuples = [tuple(x) for x in df_differences.to_numpy()]
     df_differences_columns = ','.join(list(df_differences.columns))
@@ -48,7 +47,6 @@ def insert_updates(df_differences, update, table):
         with conn.cursor() as curs:
             for row in tuples:
                 query_string = f"INSERT INTO {table} ({df_differences_columns}) VALUES %s;"
-
                 curs.execute(query_string, (row,))
             curs.execute(
                 "UPDATE citybikes.edl SET processed = TRUE WHERE id = %s",
@@ -96,7 +94,7 @@ def networks_to_edw():
     try:
         with DBConnection(db_creds()).conn as conn:
             networks_updates_count = get_updates_count(conn, 'networks')
-    except Exception as e:
+    except Exception:
         logging.error(traceback.format_exc())
         return
     print(
@@ -109,7 +107,7 @@ def networks_to_edw():
         try:
             with DBConnection(db_creds()).conn as conn:
                 networks_updates = fetch_updates(conn, 'networks')
-        except Exception as e:
+        except Exception:
             logging.error(traceback.format_exc())
             return
         # each response == a table ENTRY/ROW, so need to choose correct index/tuple(column), then select key from resulting dict
@@ -118,32 +116,40 @@ def networks_to_edw():
             try:
                 df_differences = process_network_updates(update, columns)
                 insert_updates(df_differences, update, 'citybikes.networks')
-            except Exception as e:
+            except Exception:
                 logging.error(traceback.format_exc())
         print(f"[load.networks_to_edw()] {file_count} files processed.")
 
 
 def process_station_updates(update, columns, network):
+    # get new json, compare to db
+    # return differences, if any
+
+    # gets list of last update, for each (station,network)
     with DBConnection(db_creds()).conn as conn:
         df_fromdb2 = fetch_station_db_data(
             conn, 'citybikes.stations', columns, filter=f"network='{network}'"
         )
 
+    # parse json for 'station' info then left join with db results
     df_fromjson = pd.DataFrame(update[1]['network'].get('stations'))
-    df_fromjson = df_fromjson[df_fromdb2.columns]
+    df_fromjson = df_fromjson.merge(df_fromdb2, how="left", on='id')
 
-    # append records from json
-    df_appended = pd.concat([df_fromdb2, df_fromjson], ignore_index=True)
-    df_appended = df_appended.drop_duplicates(
-        subset=['id', 'free_bikes', 'empty_slots'], keep=False
+    # remove rows that show no change since last update
+    df_differences = df_fromjson[
+        (df_fromjson.free_bikes_x != df_fromjson.free_bikes_y)
+        | (df_fromjson.empty_slots_x != df_fromjson.empty_slots_y)
+    ]
+
+    df_differences = df_differences.rename(
+        columns={'free_bikes_x': 'free_bikes', 'empty_slots_x': 'empty_slots'}
     )
-    df_differences = df_appended.drop_duplicates(subset=['id'], keep='last')
     df_differences = df_differences[df_fromdb2.columns]
-    if df_differences.empty == False:
+
+    # Add metadata
+    if df_differences.empty is False:
         df_differences['network'] = network
-        df_differences['dateApiCalled'] = update[
-            2
-        ]  # adding third column from edl
+        df_differences['dateApiCalled'] = update[2]
     return df_differences
 
 
@@ -154,16 +160,20 @@ def station_to_edw(network):
         'empty_slots',
     ]
 
-    print("[load.stations_to_edw()] querying db....")
+    print(f"[load.stations_to_edw({network})] querying db....")
     try:
         with DBConnection(db_creds()).conn as conn:
             station_updates_count = get_updates_count(conn, network)
-    except Exception as e:
+    except Exception:
         logging.error(traceback.format_exc())
         return
 
-    print(f"[load.stations_to_edw()] {station_updates_count} updates found...")
-    print("[load.stations_to_edw()] looping through results from db....")
+    print(
+        f"[load.stations_to_edw({network})] {station_updates_count} updates found..."
+    )
+    print(
+        f"[load.stations_to_edw({network})] looping through results from db...."
+    )
 
     file_count = 0
 
@@ -172,7 +182,7 @@ def station_to_edw(network):
         try:
             with DBConnection(db_creds()).conn as conn:
                 station_updates = fetch_updates(conn, network)
-        except Exception as e:
+        except Exception:
             logging.error(traceback.format_exc())
             return
         for update in station_updates:
@@ -185,9 +195,11 @@ def station_to_edw(network):
                 insert_updates(
                     df_differences, update, table='citybikes.stations'
                 )
-            except Exception as e:
+            except Exception:
                 logging.error(traceback.format_exc())
-        print(f"[load.stations_to_edw()] {file_count} files processed.")
+        print(
+            f"[load.stations_to_edw({network})] {file_count} files processed."
+        )
 
 
 def run():
